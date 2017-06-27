cbc03a2504ffd521c5205b44265e8bea375727c6
Oliver Frick <ofrick@bluewin.ch>
2017-06-27 22:23:28 +0200

    myalert - Initial commit

---
+++ myalert/src/main/java/ch/kko/hel/services/itservices/myalert/HelloWorld.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ch.kko.hel.services.itservices.myalert;
+
+/**
+ *
+ * @author oliver
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        Greeter greeter = new Greeter();
+        System.out.println(greeter.sayHello());
+    }
+}
